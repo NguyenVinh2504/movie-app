5b1d5f8b05a9792c1b102cf41f63f70f6f088c4b
NguyenVinh2504 <vinhhoang010190@gmail.com>
2023-12-11 11:37:58 +0700

    fix API ROOT

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const API_ROOT = 'http://localhost:3000';
-// export const API_ROOT = 'http://localhost:2504';
+// export const API_ROOT = 'http://localhost:3000';
+export const API_ROOT = 'http://localhost:2504';
