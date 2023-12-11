5106e8251a270edb806b8d79e9ec6035ee5c8b1c
NguyenVinh2504 <vinhhoang010190@gmail.com>
2023-12-11 21:13:06 +0700

    fix API ROOT

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,7 +4,7 @@ console.log(process.env.REACT_APP_BUILD_MODE);
 if (process.env.REACT_APP_BUILD_MODE === 'dev') {
     apiRoot = env.API_URL_DEV;
 }
-if (process.env.BUILD_MODE === 'production') {
+if (process.env.REACT_APP_BUILD_MODE === 'production') {
     apiRoot = env.API_URL_PROD;
 }
 export const API_ROOT = apiRoot;
