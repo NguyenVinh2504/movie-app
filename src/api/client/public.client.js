fbab34c73e8a705a0b956d2b9091d0c558569436
NguyenVinh2504 <vinhhoang010190@gmail.com>
2023-12-11 21:04:54 +0700

    check API ROOT

--- src/api/client/public.client.js
+++ src/api/client/public.client.js
@@ -3,6 +3,7 @@ import queryString from 'query-string';
 import { API_ROOT } from '~/utils/constants';
 
 const baseURL = `${API_ROOT}/api/v1/`;
+console.log(API_ROOT);
 
 const publicClient = axios.create({
     baseURL,
