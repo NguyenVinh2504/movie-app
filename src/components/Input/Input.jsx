277dd080a804fb9d1ab791bda1c75c521493611c
NguyenVinh2504 <vinhhoang010190@gmail.com>
2024-02-26 22:02:15 +0700

    fix input password

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -95,7 +95,7 @@ const Input = forwardRef(
                         }}
                         autoComplete="off"
                     ></input>
-                    {props?.type === 'password' && (
+                    {props?.type === 'password' && props?.value && (
                         <Box component={'span'} sx={{ display: 'flex', alignItems: 'center', cursor: 'pointer' }} onClick={toggleEye}>
                             {!openEye ? <EyeIcon /> : <EyeSlashIcon/>}
                         </Box>
