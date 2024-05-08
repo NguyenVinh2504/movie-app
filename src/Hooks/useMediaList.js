b74ecbe99942bcde03a8ad98cbf560a5cc05a9db
NguyenVinh2504 <vinhhoang010190@gmail.com>
2024-05-08 15:16:16 +0700

    sữa vào lặp danh sách yêu thích

--- src/Hooks/useMediaList.js
+++ src/Hooks/useMediaList.js
@@ -3,10 +3,14 @@ import { favoritesValue } from "~/redux/selectors";
 
 const useMediaList = (data) => {
     const favorites = useSelector(favoritesValue);
-
+    const mapFavorites = favorites.reduce((map, favorite) => {
+        map[favorite.mediaId] = favorite
+        return map
+    }, {})
     data?.pages?.forEach((itemList) => {
         itemList.results.forEach((itemResult) => {
-            const isFavorite = favorites.find((element) => element.mediaId === itemResult.id);
+            const isFavorite = mapFavorites[itemResult.id]
+            // const isFavorite = favorites.find((element) => element.mediaId === itemResult.id);
             if (isFavorite) {
                 itemResult.isFavorite = true;
                 itemResult.favoriteId = isFavorite._id;
